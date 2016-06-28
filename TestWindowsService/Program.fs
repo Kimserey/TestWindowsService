a5cf760aef68df735c74f7910470e85648b75934
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-28 23:59:53 +0100

    commited TownCrier sample topshelf

--- TestWindowsService/Program.fs
+++ TestWindowsService/Program.fs
@@ -1,7 +1,37 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.Timers
+open System.Diagnostics
+open Topshelf
+open Topshelf.ServiceConfigurators
+open Topshelf.HostConfigurators
+
+type TownCrier() =
+    let timer = new Timer(1000.)
+    do timer.Elapsed.Add(fun args -> printfn "It is %s and all is well" (DateTime.Now.ToLongTimeString()))
+
+    member x.Start() = timer.Start()
+    member x.Stop() = timer.Stop()
+
+let runService (hostCfg: HostConfigurator) = 
+
+    hostCfg.Service<TownCrier>(Action<_>(fun (s: ServiceConfigurator<TownCrier>) ->
+        s.ConstructUsing(Func<TownCrier>(fun () -> new TownCrier()))
+            .WhenStarted(Action<TownCrier>(fun s -> s.Start()))
+            .WhenStopped(Action<TownCrier>(fun s -> s.Stop()))
+            |> ignore )) |> ignore
+    
+    hostCfg.RunAsLocalSystem() |> ignore
+    hostCfg.SetDescription("Sample topshelf host")
+    hostCfg.SetDisplayName("Display name")
+    hostCfg.SetServiceName("Service name")
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+    HostFactory.Run(Action<_>(runService))
+    |> ignore
+
+    0
